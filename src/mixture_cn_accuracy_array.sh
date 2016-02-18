abf63c3ede6b8cbc36e01b651c7b84aa375a33dd
Yutaro Konta <yk@mbp.local>
2016-02-18 12:06:45 +0900

    modified to array job

--- src/mixture_cn_accuracy_array.sh
+++ src/mixture_cn_accuracy_array.sh
@@ -9,12 +9,6 @@ echo SGE_TASK_FIRST:$SGE_TASK_FIRST
 echo SGE_TASK_LAST:$SGE_TASK_LAST
 echo SGE_TASK_STEPSIZE:$SGE_TASK_STEPSIZE
 
-n=1
-
-while [ $a -lt 1000 ]
-do
-    echo $a
-    ./mixture_cn_generate `printf "mixture_cn_generate.out%d" $a` 100000 $a 4 kappa.txt
-    ./mixture_cn_accuracy 4 4 $a `printf "mixture_cn_generate.out%d" $a` `printf "mixture_cn_accuracy.out%d" $a` kappa.txt mixture_cn_accuracy.accuracy
-    a=`expr $a * 4`
-done
+n=`expr 10 * $SGE_TASK_ID`
+./mixture_cn_generate `printf "mixture_cn_generate.out%d" $a` 100000 $n 4 kappa.txt
+./mixture_cn_accuracy 4 4 $n `printf "mixture_cn_generate.out%d" $a` `printf "mixture_cn_accuracy.out%d" $a` kappa.txt mixture_cn_accuracy.accuracy
