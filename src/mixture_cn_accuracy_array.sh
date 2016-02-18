06afff1c830886e402b4ac48f61459755bb81ba9
Yutaro Konta <yk@mbp.local>
2016-02-18 12:13:46 +0900

    corrected expr

--- src/mixture_cn_accuracy_array.sh
+++ src/mixture_cn_accuracy_array.sh
@@ -9,6 +9,6 @@ echo SGE_TASK_FIRST:$SGE_TASK_FIRST
 echo SGE_TASK_LAST:$SGE_TASK_LAST
 echo SGE_TASK_STEPSIZE:$SGE_TASK_STEPSIZE
 
-n=`expr 10 * $SGE_TASK_ID`
+n=`expr 10 \* $SGE_TASK_ID`
 ./mixture_cn_generate `printf "mixture_cn_generate.out%d" $a` 100000 $n 4 kappa.txt
 ./mixture_cn_accuracy 4 4 $n `printf "mixture_cn_generate.out%d" $a` `printf "mixture_cn_accuracy.out%d" $a` kappa.txt mixture_cn_accuracy.accuracy
