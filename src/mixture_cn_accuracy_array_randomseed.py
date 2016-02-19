e9757d11b657329de2e068cd5e67b176e35debb8
Yutaro Konta <yk@mbp.local>
2016-02-19 20:28:07 +0900

    write to different file to avoid collision

--- src/mixture_cn_accuracy_array_randomseed.py
+++ src/mixture_cn_accuracy_array_randomseed.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
-f = open("mixture_cn_accuracy_randomseed.accuracy", "r")
-g = open("mixture_cn_accuracy_randomseed.accuracy.mean", "w")
-
 dic = {}
 count = {}
-lines = f.readlines()
-num = len(lines)
-for line in lines:
-    item = line.rstrip('\n').split('\t')
-    n = int(item[0])
-    err = float(item[1])
-    if n in dic.keys():
-        dic[n] += err
-        count[n] += 1
-    else:
-        dic[n] = err
-        count[n] = 1
+for i in range(100):
+    f = open("mixture_cn_accuracy_randomseed.accuracy" + str(i+1), "r")
+    for line in f.readlines():
+        item = line.rstrip('\n').split('\t')
+        n = int(item[0])
+        err = float(item[1])
+        if n in dic.keys():
+            dic[n] += err
+            count[n] += 1
+        else:
+            dic[n] = err
+            count[n] = 1
+    f.close()
 
+g = open("mixture_cn_accuracy_randomseed.accuracy.mean", "w")
 for n in sorted(dic.keys()):
     g.write("%d\t%f\n" % (n, dic[n] / count[n]))
+g.close()
