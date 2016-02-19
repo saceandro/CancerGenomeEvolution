2e1d4baa62006fdc477794b71018bf67d78114fb
Yutaro Konta <yk@mbp.local>
2016-02-19 16:09:23 +0900

    modified:   src/mixture_cn_accuracy_array_randomseed.sh

--- src/mixture_cn_accuracy_array_randomseed.sh
+++ src/mixture_cn_accuracy_array_randomseed.sh
@@ -10,6 +10,6 @@ echo SGE_TASK_LAST:$SGE_TASK_LAST
 echo SGE_TASK_STEPSIZE:$SGE_TASK_STEPSIZE
 
 remainder=`expr $SGE_TASK_ID / 10`
-n=`expr 10 \* remainder`
+n=`expr 10 \* $remainder`
 ./mixture_cn_generate_randomseed `printf "mixture_cn_generate_randomseed.out%d" $SGE_TASK_ID` 100000 $n 4 kappa.txt
 ./mixture_cn_accuracy 4 4 $n `printf "mixture_cn_generate_randomseed.out%d" $SGE_TASK_ID` kappa.txt mixture_cn_accuracy_randomseed.accuracy
