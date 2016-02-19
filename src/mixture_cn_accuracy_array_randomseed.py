ada7b35159974c546ded74f31f14fe172bb85ee3
Yutaro Konta <yk@mbp.local>
2016-02-19 17:34:23 +0900

    added python script

---
+++ src/mixture_cn_accuracy_array_randomseed.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+f = open("mixture_cn_accuracy_randomseed.accuracy", "r")
+g = open("mixture_cn_accuracy_randomseed.accuracy.mean", "w")
+
+dic = {}
+lines = f.readlines()
+num = len(lines)
+for line in lines:
+    item = line.split("\t").rstrip()
+    n = froat(item[0])
+    err = froat(item[1])
+    if n in dic:
+        dic[n] += err
+    else:
+        dic[n] = 0
+
+for n in dic.keys():
+    print "%i\t%d\n" % (n, dic[n] / num)
