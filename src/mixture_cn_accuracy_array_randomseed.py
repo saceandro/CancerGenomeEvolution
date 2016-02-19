656e7ea8a83bad7ce2a1a54b2e0e12cfaf5f8910
Yutaro Konta <yk@mbp.local>
2016-02-19 18:12:08 +0900

    modified:   src/mixture_cn_accuracy_array_randomseed.py modified:   src/mixture_cn_accuracy_array_randomseed.sh

--- src/mixture_cn_accuracy_array_randomseed.py
+++ src/mixture_cn_accuracy_array_randomseed.py
@@ -4,16 +4,19 @@ f = open("mixture_cn_accuracy_randomseed.accuracy", "r")
 g = open("mixture_cn_accuracy_randomseed.accuracy.mean", "w")
 
 dic = {}
+count = {}
 lines = f.readlines()
 num = len(lines)
 for line in lines:
     item = line.rstrip('\n').split('\t')
-    n = froat(item[0])
-    err = froat(item[1])
-    if n in dic:
+    n = int(item[0])
+    err = float(item[1])
+    if n in dic.keys():
         dic[n] += err
+        count[n] += 1
     else:
-        dic[n] = 0
+        dic[n] = err
+        count[n] = 1
 
-for n in dic.keys():
-    print "%i\t%d\n" % (n, dic[n] / num)
+for n in sorted(dic.keys()):
+    g.write("%d\t%f\n" % (n, dic[n] / count[n]))
--- src/mixture_cn_accuracy_array_randomseed.sh
+++ src/mixture_cn_accuracy_array_randomseed.sh
@@ -11,7 +11,8 @@ echo SGE_TASK_FIRST:$SGE_TASK_FIRST
 echo SGE_TASK_LAST:$SGE_TASK_LAST
 echo SGE_TASK_STEPSIZE:$SGE_TASK_STEPSIZE
 
-remainder=`expr $SGE_TASK_ID \/ 10`
+remainder=`expr $SGE_TASK_ID % 10 + 1`
+echo remainder:$remainder
 n=`expr 10 \* $remainder`
 ./mixture_cn_generate_randomseed `printf "./mixture_cn_io/mixture_cn_generate_randomseed.out%d" $SGE_TASK_ID` 100000 $n 4 kappa.txt $SGE_TASK_ID
 ./mixture_cn_accuracy_randomseed 4 4 $n `printf "./mixture_cn_io/mixture_cn_generate_randomseed.out%d" $SGE_TASK_ID` kappa.txt mixture_cn_accuracy_randomseed.accuracy
