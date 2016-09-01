7450ac841aa26c179cf2c12fc470b832a815b08f
Konta_Yutaro <konta@sc093.(none)>
2016-09-01 16:12:51 +0900

    completed alpha_beta_map_same_cn_compare_gradient.sh

--- src/alpha_beta_map_same_cn/alpha_beta_map_same_cn_compare_gradient.sh
+++ src/alpha_beta_map_same_cn/alpha_beta_map_same_cn_compare_gradient.sh
@@ -2,3 +2,9 @@
 #$ -S /usr/local/bin/zsh
 #$ -cwd
 #$ -N alpha_beta_map_same_cn_compare_gradient
+
+for ((j = 0; j < 5; j++)); do
+    for ((i = 1; i <= 4; i++)); do
+        ./alpha_beta_map_same_cn_compare_gradient 4 10 2 alpha_beta_same_cn_generate.random.reads10 result/alpha_beta_map_same_cn_compare_gradient.4_10_2_random_reads.${j}_${i}.u $j $i;
+    done
+done
