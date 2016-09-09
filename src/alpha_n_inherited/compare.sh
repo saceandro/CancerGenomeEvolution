609c6a92cede9dfa9c16482bdb2dd4ff0becae73
Yutaro Konta <yk@mbp-6.local>
2016-09-10 03:37:19 +0900

    modified compare.sh

--- src/alpha_n_inherited/compare.sh
+++ src/alpha_n_inherited/compare.sh
@@ -5,6 +5,6 @@
 
 for ((uindex = 1; uindex<=4; uindex++)); do
     for ((nindex = 1; nindex<=4; nindex++)); do
-        ./compare 4 203 2 0.97547454735 4_2_3_10000_100_1.reads_allinherited.true_inherited compare_result3/u.${uindex} compare_result3/n.${nindex} 2 $uindex $nindex > compare_result3/log.${uindex}_${nindex}
+        ./compare 4 1000 2 0.97547454735 generated/4_2_3_10000_100000.reads compare_result4/u.${uindex} compare_result4/n.${nindex} 2 $uindex $nindex > compare_result4/log.${uindex}_${nindex}
     done
 done
